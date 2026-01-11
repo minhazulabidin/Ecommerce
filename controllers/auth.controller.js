9dd7697f99a6777a1778b287c75d54a57c832c1b
minhazulabidin <minhajulabedin648@gmail.com>
2026-01-11 16:17:10 +0600

    signup post method done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,15 @@
-exports.signupController = (req, res) => {
-    res.send('User signup');
+const userModel = require("../models/user.model");
+
+exports.signupController = async (req, res) => {
+    const { fullName, email, password, phone, address } = req.body;
+
+    const user = new userModel({
+        fullName, email, password, phone, address
+    })
+    await user.save()
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        user
+    });
 }
--- route/api/auth/index.js
+++ route/api/auth/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { signupController } = require('../../../controllers/auth.controller');
 const router = express.Router();
 
-
+//localhost:8080/api/v1/auth/signup
 router.post('/signup', signupController)
 
 module.exports = router;
