5cad9d813506e3f5a92ce49997f78e3ba6fb1db0
introtoxicXXl <minhajulabedin648@gmail.com>
2026-02-15 22:29:33 +0600

    category schema model basic create

---
+++ model/category.model.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+
+const categorySchema = new mongoose.Schema({
+
+},
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model("Category", categorySchema)
