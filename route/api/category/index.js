fb17945a12d8f3ad684d43373bd8a8b6e3831a73
minhazulabidin <minhajulabedin648@gmail.com>
2026-02-20 11:10:59 +0600

    add category authorize done

--- route/api/category/index.js
+++ route/api/category/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { addCategoryController, allCategoryController } = require('../../../controllers/categoryController');
 const upload = require('../../../utilities/upload');
+const { isAuthorize } = require('../../../middleware/isAuthorize');
+const { isAuthorizeRole } = require('../../../middleware/isAuthorizeRole');
 const router = express.Router()
 
-router.post('/add-category', upload.single("image"), addCategoryController)
+router.post('/add-category', isAuthorize, isAuthorizeRole("admin"), upload.single("image"), addCategoryController)
 router.get('/allCategory', allCategoryController)
 
 module.exports = router;
