e9c13f1aadf7ae4703487991931302a7cf41992a
minhazulabidin <minhajulabedin648@gmail.com>
2026-02-20 12:04:48 +0600

    update category with image done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
 const categoryModel = require("../model/category.model");
 const { apiResponse } = require("../utilities/apiResponse");
 const { asyncController } = require("../utilities/asyncController");
+const path = require("path");
+const fs = require("fs");
 
 exports.addCategoryController = asyncController(async (req, res) => {
     const { name, subCategory, discount } = req.body
@@ -23,9 +25,27 @@ exports.allCategoryController = asyncController(async (req, res) => {
 exports.updateCategoryController = asyncController(async (req, res) => {
     const { id } = req.params;
     const { name, subCategory, discount } = req.body;
-    const { filename } = req.file || {};
-    if (filename) {
-        
+    const { filename } = req.file;
+    if (req.file) {
+        const category = await categoryModel.findOne({ _id: id })
+        const filePath = category.image.split("/")
+        const imagePath = filePath[filePath.length - 1]
+        const oldPath = path.join(__dirname, "../uploads")
+        // console.log(`${oldPath}/${imagePath}`);
+        fs.unlink(`${oldPath}/${imagePath}`, async (err) => {
+            if (err) {
+                return apiResponse(500, res, err.message)
+            } else {
+                const image = `${process.env.SEVER_URL}/${filename}`
+                category.image = image
+                category.subCategory = subCategory
+                category.discount = discount
+                category.name = name
+                await category.save()
+                apiResponse(200, res, "Category updated successfully", category)
+            }
+        })
+
     } else {
         const update = await categoryModel.findOneAndUpdate({ _id: id }, { name, subCategory, discount }, { new: true })
         apiResponse(200, res, "Category updated successfully", update)
