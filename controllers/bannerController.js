1aaffdb26aa68af2d045f84dd3b41d01d3cecb52
minhazulabidin <minhajulabedin648@gmail.com>
2026-02-22 14:37:53 +0600

    banner update done

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -1,6 +1,8 @@
 const { asyncController } = require("../utilities/asyncController");
 const bannerModel = require("../model/banner.model");
 const { apiResponse } = require("../utilities/apiResponse");
+const path = require("path");
+const fs = require("fs");
 
 exports.addBannerController = asyncController(async (req, res) => {
     const { url } = req.body;
@@ -12,4 +14,22 @@ exports.addBannerController = asyncController(async (req, res) => {
     await banner.save()
     apiResponse(201, res, "Banner added successfully", banner)
 
+})
+
+exports.updateBannerController = asyncController(async (req, res) => {
+    const { id } = req.params;
+    const { url } = req.body;
+    const banner = await bannerModel.findById(id);
+    if (url !== undefined) banner.url = url;
+    if (req.file !== undefined) {
+        const { filename } = req.file;
+
+        const filePath = banner.image.split("/");
+        const imagePath = filePath[filePath.length - 1];
+        const oldPath = path.join(__dirname, "../uploads", imagePath)
+        await fs.promises.unlink(oldPath);
+        banner.image = `${process.env.SEVER_URL}/${filename}`;
+    }
+    await banner.save();
+    apiResponse(200, res, "Banner updated successfully", banner);
 })
--- route/api/banner/index.js
+++ route/api/banner/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { addBannerController } = require("../../../controllers/bannerController");
+const { addBannerController, updateBannerController } = require("../../../controllers/bannerController");
 const upload = require("../../../utilities/upload");
+const { isAuthorize } = require("../../../middleware/isAuthorize");
+const { isAuthorizeRole } = require("../../../middleware/isAuthorizeRole");
 const router = express.Router()
 
 router.post("/addBanner", upload.single("image"), addBannerController)
+router.patch("/updateBanner/:id", upload.single("image"), updateBannerController)
 
 module.exports = router;
