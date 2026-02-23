289b6f37d1132cfb0fe1cc2440b17259aa0c0a77
introtoxicXXl <minhajulabedin648@gmail.com>
2026-02-23 23:29:38 +0600

    approve merchent done

--- route/api/merchant/index.js
+++ route/api/merchant/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const { applyMerchantController, approveMerchantController } = require("../../../controllers/merchantController")
+const { isAuthorize } = require("../../../middleware/isAuthorize")
+const { isAuthorizeRole } = require("../../../middleware/isAuthorizeRole")
 const router = express.Router()
 
-
+router.post("/applyMerchant", applyMerchantController)
+router.patch("/approveMerchant/:id", isAuthorize, isAuthorizeRole("admin"), approveMerchantController)
 
 
 module.exports = router
