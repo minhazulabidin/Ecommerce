f07c1bd73090484f08768df1fbd84534a4cd0637
introtoxicXXl <minhajulabedin648@gmail.com>
2026-02-25 16:41:39 +0600

    get all product fetch done

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,6 +36,22 @@ exports.deleteProductController = asyncController(async (req, res) => {
             await replaceImage(item)
         })
         await productModel.deleteOne({ _id: id })
+        await categoryModel.findByIdAndUpdate(product.category, { $pull: { product: id } }, { new: true })
         apiResponse(200, res, "Product delete successfully")
+    }
+})
+
+exports.allProductController = asyncController(async (req, res) => {
+    const allProducts = await productModel.find({})
+    apiResponse(200, res, "All product fetch successfully", allProducts)
+})
+
+exports.singleProductController = asyncController(async (req, res) => {
+    const { slug } = req.params
+    const product = await productModel.findOne({ slug })
+    if (!product) {
+        apiResponse(404, res, "Product not found")
+    } else {
+        apiResponse(200, res, "Single product fetch successfully", product)
     }
 })
--- route/api/product/index.js
+++ route/api/product/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addProductController, deleteProductController } = require('../../../controllers/productController');
+const { addProductController, deleteProductController, allProductController, singleProductController } = require('../../../controllers/productController');
 const upload = require('../../../utilities/upload');
 const router = express.Router()
 
 router.post('/addProduct', upload.array("image", 5), addProductController)
 router.delete('/deleteProduct/:id', deleteProductController)
+router.get('/allProducts', allProductController)
+router.get('/singleProduct/:slug', singleProductController)
 
 module.exports = router;
