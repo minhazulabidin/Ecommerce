f5bf22c34ece1f50e2769eaa47fd3aea278f6634
introtoxicXXl <minhajulabedin648@gmail.com>
2026-04-02 23:18:16 +0600

    cloudinary image delete from deleteController set up done

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,6 +5,7 @@ const { asyncController } = require("../utilities/asyncController");
 const categoryModel = require("../model/category.model");
 const { replaceImage } = require("../helper/replaceImage");
 const uploadImage = require("../utilities/uploadImage");
+const cloudinary= require("../config/cloudinary.config");
 
 exports.addProductController = asyncController(async (req, res) => {
     const { name, price, discountPrice, rating, description, category, variantType, sku } = req.body;
@@ -40,9 +41,11 @@ exports.deleteProductController = asyncController(async (req, res) => {
     if (!product) {
         return apiResponse(404, res, "Product not found")
     } else {
-        product.image.forEach(async item => {
-            await replaceImage(item)
-        })
+        await Promise.all(
+            product.image.map(async (img) => {
+                await cloudinary.uploader.destroy(img.public_id)
+            })
+        )
         await productModel.deleteOne({ _id: id })
         await categoryModel.findByIdAndUpdate(product.category, { $pull: { product: id } }, { new: true })
         apiResponse(200, res, "Product delete successfully")
