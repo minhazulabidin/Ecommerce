a5e66cabe785977164128106fa7880145ffcebfc
minhazulabidin <minhajulabedin648@gmail.com>
2026-04-08 14:03:22 +0600

    router setup done

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: RootLayout,
+      children: [{ index: true, Component: Home }],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
