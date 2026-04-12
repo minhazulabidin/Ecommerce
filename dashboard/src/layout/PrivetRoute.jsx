f4ac502ace3d361ef99bc033eb804a23d3554443
introtoxicXXl <minhajulabedin648@gmail.com>
2026-04-12 22:59:29 +0600

    l

--- dashboard/src/layout/PrivetRoute.jsx
+++ dashboard/src/layout/PrivetRoute.jsx
@@ -1,15 +1,17 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router';
 
 export const PrivetRoute = ({ children }) => {
     const navigate = useNavigate()
 
-    axios.get(`${import.meta.env.VITE_API_URL}/auth/me`, { withCredentials: true }).then(() => {
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/auth/me`, { withCredentials: true }).then(() => {
 
-    }).catch(() => {
-        navigate("/login")
-    });
+        }).catch(() => {
+            navigate("/login")
+        });
+    }, [navigate])
 
     return (
         <>{children}</>
