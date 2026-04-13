f83e908559e746a016adf56e667046eb6b8b3b57
minhazulabidin <minhajulabedin648@gmail.com>
2026-04-13 16:43:26 +0600

    add banner and get banner done

--- dashboard/src/pages/AllBanner.jsx
+++ dashboard/src/pages/AllBanner.jsx
@@ -1,27 +1,49 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const AllBanner = () => {
+  const [banners, setBanners] = useState([]);
+
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/banner/allBanner`)
-  },[]);
+    const fetchBanners = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/banner/allBanner`,
+          {
+            withCredentials: true,
+          },
+        );
+
+        setBanners(res?.data.data || res.data || []);
+      } catch (err) {
+        console.error(err.response?.data || err.message);
+      }
+    };
+
+    fetchBanners();
+  }, []);
 
   return (
     <div className="max-w-screen py-6 mx-auto">
       <div className="space-y-6">
         <div className="grid md:grid-cols-9 gap-6">
-          <div className="relative group cursor-pointer w-full md:h-75 md:col-span-3">
-            <img
-              className="w-full h-full rounded-lg object-cover object-top"
-              src="https://readymadeui.com/images/gallery-img-1.webp"
-              alt="img-1"
-            />
-            <div className="absolute inset-0 bg-black/40 rounded-lg opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center">
-              <h3 className="text-white text-lg font-semibold">
-                Your Title Here
-              </h3>
+          {banners.map((banner) => (
+            <div
+              key={banner._id}
+              className="relative group cursor-pointer w-full md:h-75 md:col-span-3"
+            >
+              <img
+                className="w-full h-full rounded-lg object-cover object-top"
+                src={banner.image}
+                alt={`Banner ${banner._id}`}
+              />
+              <div className="absolute inset-0 bg-black/40 rounded-lg flex items-end justify-center pb-4 inset-x-0 bottom-0 p-3 bg-linear-to-t from-black/70 to-transparent rounded-b-lg opacity-0 group-hover:opacity-100 transition duration-300">
+                <h3 className="text-white text-sm text-center truncate">
+                  {banner.url || "No URL"}
+                </h3>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
