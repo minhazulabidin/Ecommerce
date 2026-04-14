15188c48dfc5dbf46978e7384850f6f77cdfd093
introtoxicXXl <minhajulabedin648@gmail.com>
2026-04-14 21:55:02 +0600

    show all category done

--- dashboard/src/pages/AllBanner.jsx
+++ dashboard/src/pages/AllBanner.jsx
@@ -4,7 +4,7 @@ import ProductsShowcase from './../Components/ui/ProductsShowcase';
 
 export const AllBanner = () => {
   const [banners, setBanners] = useState([]);
-  console.log(banners)
+
   useEffect(() => {
     const fetchBanners = async () => {
       try {
@@ -25,23 +25,23 @@ export const AllBanner = () => {
   }, []);
 
   return (
-    <div className="max-w-screen py-6 mx-auto">
-      <div className="space-y-6">
-        <div className="grid">
-          <ProductsShowcase
-            cards={banners.map((item) => ({
-              title: item._id,
-              description: "Click to show",
-              src: item.image,
-              ctaText: "Visit",
-              ctaLink: item.url,
-              content: () => (
-                <p>{item.url}</p>
-              ),
-            }))}
-          />
-        </div>
-      </div>
-    </div>
+   <div className="max-w-screen mx-auto">
+         <div className="space-y-6">
+           <div className="">
+             <ProductsShowcase
+               cards={banners.map((item) => ({
+                 title: item._id,
+                 description: "Click to show",
+                 src: item.image,
+                 ctaText: "Visit",
+                 ctaLink: item.url,
+                 content: () => (
+                   <p>{item.url}</p>
+                 ),
+               }))}
+             />
+           </div>
+         </div>
+       </div>
   );
 };
--- dashboard/src/pages/AllCategory.jsx
+++ dashboard/src/pages/AllCategory.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import CategoryShowCase from './../Components/ui/CategoryShowCase';
 
 export const AllCategory = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/category/categories`,
+          {
+            withCredentials: true,
+          },
+        );
+
+        setCategories(res?.data.data || res.data || []);
+      } catch (err) {
+        console.error(err.response?.data || err.message);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+  console.log(categories)
+
   return (
-    <div>AllCategory</div>
+    <main className="w-9/12 mx-auto">
+      <CategoryShowCase
+        cards={categories.map((item) => ({
+          title: item.name,
+          description: item.slug,
+          src: item.image,
+          ctaText: "Visit",
+          ctaLink: item.url,
+          product: item.product,
+          subCategory: item.subCategory,
+          discount: item.discount,
+          content: () => (
+            <p>{item.url}</p>
+          ),
+        }))}
+      />
+    </main>
   )
 }
